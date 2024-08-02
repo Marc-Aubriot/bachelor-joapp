d21f16e5f038dea6971c0f94daa4947ac1e8e931
Aubriot Marc <marc.aubriot@outlook.fr>
2024-08-02 21:47:30 +0200

    build homepage, ticket page, login / register page

--- public/build/assets/Tickets-DLmX-E7M.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as c,e as o}from"./app-z4YC1Cky.js";const r={};function t(n,s){return c(),o("h1",null,"TICKETS")}const f=e(r,[["render",t]]);export{f as default};
---
+++ public/build/assets/utilities-D0713FlC.js
@@ -0,0 +1,1 @@
+const s=t=>`http://[::1]:5173/public/assets/${t}`;export{s as g};
