bcd3a5142fbf1e460dae609b70503dfe98d5557e
Aubriot Marc <marc.aubriot@outlook.fr>
2024-08-13 22:58:05 +0200

    building version 1.4

--- public/build/assets/AuthenticationCard-ClMpScGa.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as r,e as t,r as s}from"./app-DZ5Hud-q.js";const n={},a={class:"w-full sm:max-w-md rounded-xl px-6 py-4 bg-white shadow-md overflow-hidden"};function c(e,d){return r(),t("div",a,[s(e.$slots,"default")])}const m=o(n,[["render",c]]);export{m as A};
--- resources/js/utilities.js
+++ resources/js/utilities.js
@@ -8,8 +8,8 @@ import 'vue3-toastify/dist/index.css';
  */
 
 const getImgPath = (img) => {
-    //return `https://dev4a.online/public/assets/${img}`;   //  PROD
-    return `http://[::1]:5173/public/assets/${img}`;  //  LOCAL
+    return `https://dev4a.online/public/assets/${img}`;   //  PROD
+    //return `http://[::1]:5173/public/assets/${img}`;  //  LOCAL
 };
 
 /**
