d8ed73eaf6db0352ee6a9bcabf2a4d9b6851f0c4
Aubriot Marc <marc.aubriot@outlook.fr>
2024-09-15 15:30:53 +0200

    CORS config 1

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['dev4a.online.com'],
 
     'allowed_origins_patterns' => [],
 
